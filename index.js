ad48b5fbcca1973534a87abe6ffce4c521b08622
rzn0v <126226011+rzn0v@users.noreply.github.com>
2025-08-07 23:54:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function playGame(){
+    var urMove = Math.ceil(Math.random()*6);
+    var computerMove = Math.ceil(Math.random()*6);
+    document.querySelector(".img1").setAttribute("src",`/images/dice${urMove}.png`);
+    document.querySelector(".img2").setAttribute("src",`/images/dice${computerMove}.png`);
+
+    if(urMove === computerMove){
+        document.querySelector("h1").innerHTML = "It's a draw";
+    }else if(computerMove > urMove){
+        document.querySelector("h1").innerHTML = "You lost!";
+    }else{
+        document.querySelector("h1").innerHTML = "You won!";
+    }
+}
